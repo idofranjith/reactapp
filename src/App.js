5c159d62a6167b1ec9d0d79ec1f4e9fd6853b5c6
Ranjithkumar Annamalai <r.annamalai@spglobal.com>
2021-03-02 23:52:41 +0530

    firstapp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Greet from './components/Greet'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+              <Greet />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Greet extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1 fontcolor> Hi Ranjith Kumar </h1>
+            </div>
+        )
+    }
+}
+
+export default Greet
